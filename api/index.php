423fdb291121a094b696e2e46c4d6ad97feeca16
anestiandinesta <anestiandi05@gmail.com>
2026-03-13 22:03:47 +0700

    fix(vercel): configure read-only filesystem and build assets

--- api/index.php
+++ api/index.php
@@ -1,4 +1,10 @@
 <?php
 
-// Forward Vercel requests to normal index.php
+// Konfigurasi Vercel read-only filesystem
+putenv('APP_DEBUG=true'); 
+putenv('LOG_CHANNEL=stderr');
+putenv('CACHE_DRIVER=array');
+putenv('SESSION_DRIVER=cookie'); 
+putenv('VIEW_COMPILED_PATH=/tmp');
+
 require __DIR__ . '/../public/index.php';
